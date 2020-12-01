9d20df6bcf89835dd2ada843f947683c74cc6f1c
santh <santhanamvenkatram@gmail.com>
2020-12-01 21:43:12 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class hello{
+public static void main(String[] args) {
+ 
+    
+Scanner s=new Scanner(System.in);
+int sa=s.nextInt();
+String k="helloworld";
+for(int i=0;i<sa;i++){
+    System.out.println("dummil"+"\n");
+}
+System.out.println(k+" "+sa);
+}
+}
